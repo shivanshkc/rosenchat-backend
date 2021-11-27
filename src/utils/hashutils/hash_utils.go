3d6eaa0e3f7384b11168e88522800f1d8561e8d9
Shivansh Kuchchal <skuchcha@redhat.com>
2021-11-27 12:45:28 +0530

    Using SHA256 instead of SHA1.

--- src/utils/hashutils/hash_utils.go
+++ src/utils/hashutils/hash_utils.go
@@ -1,13 +1,13 @@
 package hashutils
 
 import (
-	"crypto/sha1"
+	"crypto/sha256"
 	"encoding/hex"
 )
 
 // SHA256Hex provides the hex encoded SHA256 of the input.
 func SHA256Hex(input string) string {
-	hash := sha1.New()
+	hash := sha256.New()
 	hash.Write([]byte(input))
 	return hex.EncodeToString(hash.Sum(nil))
 }
